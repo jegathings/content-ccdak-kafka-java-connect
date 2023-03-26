4caccc47d64d2ed7d69d79d0c0d2d55bc87f8546
James Gathings <jegathings@gmail.com>
2023-03-26 16:00:59 -0400

    Initial commit

--- src/main/java/com/linuxacademy/ccdak/kafkaJavaConnect/Main.java
+++ src/main/java/com/linuxacademy/ccdak/kafkaJavaConnect/Main.java
@@ -1,8 +1,23 @@
 package com.linuxacademy.ccdak.kafkaJavaConnect;
-
+import org.apache.kafka.clients.producer.*;
+import java.util.Properties;
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        for (int i = 0; i < 100; i++) {
+            producer.send(
+                new ProducerRecord<String, String>("count-topic", "count", Integer.toString(i)));
+        }
+        System.out.println("Here1");
+        try{
+            producer.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        System.out.println("Here2");
     }
 }
